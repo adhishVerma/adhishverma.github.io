ffa7950c58fdc23c2d8a7c49f35042df74f58fb3
adhishVerma <v.adhish16@gmail.com>
2024-03-10 22:28:05 +0530

    add: dialog function, fix:ui changes

--- app.js
+++ app.js
@@ -1,3 +1,75 @@
+// projects arr
+const projectsArr = [];
+
+// dialog box manipulation
+const projectModal = document.querySelector('#project-modal');
+const closeButton = document.querySelector('[data-close-modal]');
+const modalName = document.querySelector("#modal-name");
+const modalDesc = document.querySelector("#modal-desc");
+const carousel = document.querySelector('#carousel-main');
+const leftArrow = document.getElementById('arrow-left');
+const rightArrow = document.getElementById('arrow-right');
+
+
+// modal selectors
+const modalTitle = document.querySelector(`#modal-title`);
+const modalLink = document.querySelector(`#modal-live-link`);
+const modalGit = document.querySelector('#modal-github-link');
+
+
+
+let index = 0
+
+function populateAndShowModal(event) {
+
+  // get the event listener data
+  const { index } = event.target.dataset;
+  const project = projectsArr[index];
+
+  // populate the modal
+  modalTitle.innerText = project.title;
+  modalLink.setAttribute('href', project.link);
+
+  modalGit.setAttribute('href', project.github);
+
+  modalDesc.innerText = project.desc;
+  modalName.innerText = project.title;
+  const images = project.images
+  images.forEach((imageSrc) => {
+    const img = document.createElement('img');
+    img.src = imageSrc
+    img.classList.add('carousel-img');
+
+    carousel.appendChild(img);
+    slides.push(img);
+  })
+  // show the dialog
+  showSlide(0);
+  projectModal.showModal();
+}
+
+// modal change slide
+leftArrow.addEventListener('click', function () {
+  index -= 1
+  showSlide(index);
+})
+
+rightArrow.addEventListener('click', function () {
+  index += 1
+  showSlide(index);
+})
+
+
+// modal close.
+closeButton.addEventListener('click', function (event) {
+  carousel.innerHTML = ""
+  index = 0
+  slides = []
+  projectModal.close();
+})
+
+
+// blob animation
 const blob = document.getElementById("blob");
 
 document.body.onpointermove = (event) => {
@@ -67,20 +139,69 @@ fetch("./static/data.json")
   .then((res) => res.json())
   .then((data) => {
     data.forEach((project, i) => {
-      let card = `<div class="col-md-6 col-xxl-4 my-3">
-                <div class="img-container">
-                <img src="${project.img}" alt="" loading="lazy"/></div>
-                <div class="project-info">
-                <p class="project-title"><a href="${project.link}" target="blank_">${project.title}</a></p>
-                <div class="project-link"><a href="${project.github}" target="blank_">Github <i class="fa-brands fa-github fa-sm" style="color: #f9f9f9;"></i></a></div></div>
-                </div>`;
 
-      projects.innerHTML += card;
+      // let card = `<div class="col-md-6 col-xxl-4 my-3">
+      //           <div class="img-container">
+      //           <img src="${project.img}" alt="" loading="lazy" onclick="(e) => console.log(e)"/></div>
+      //           <div class="project-info">
+      //           <p class="project-title"><a href="${project.link}" target="blank_">${project.title}</a></p>
+      //           <div class="project-link"><a href="${project.github}" target="blank_">Github <i class="fa-brands fa-github fa-sm" style="color: #f9f9f9;"></i></a></div></div>
+      //           </div>`;
+
+      projectsArr.push(project);
+
+      const card = document.createElement('div');
+      card.classList.add('col-md-6', 'col-xxl-4', 'my-3');
+
+      const imgContainer = document.createElement('div');
+      const img = document.createElement('img');
+      img.setAttribute('loading', 'lazy');
+      img.setAttribute('src', project.img);
+      img.dataset.index = i;
+      img.onclick = (e) => populateAndShowModal(e);
+      imgContainer.classList.add('img-container');
+      imgContainer.appendChild(img);
+      card.appendChild(imgContainer);
+
+      const projectInfo = document.createElement('div');
+      projectInfo.classList.add('project-info');
+      card.appendChild(projectInfo);
+
+      const projectTitle = document.createElement('p');
+      projectTitle.classList.add('project-title');
+
+
+      const projectLive = document.createElement('a');
+      projectLive.innerText = project.title;
+      projectLive.setAttribute('target', 'blank_');
+      projectLive.href = project.link;
+
+      projectTitle.appendChild(projectLive);
+      projectInfo.appendChild(projectTitle);
+
+      projects.appendChild(card);
     });
   });
 
-// tab-switching
 
+let slides = []
+
+function showSlide(x) {
+  if (x >= slides.length) {
+    index = 0;
+  } if (x < 0) {
+    index = slides.length - 1;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    if (i === index) {
+      slides[i].style.visibility = "unset";
+    } else {
+      slides[i].style.visibility = "hidden";
+    }
+  }
+}
+
+// tab-switching
 const onTabClick = (event) => {
   let activeTabs = document.querySelectorAll(".active");
 
@@ -127,26 +248,20 @@ contactLink.addEventListener("mouseover", (event) => {
 });
 
 
-gsap.from("#social-icons",{
-  opacity : 0,
-  scrollTrigger : {
-    scrub : 1
+gsap.from("#social-icons", {
+  opacity: 0,
+  scrollTrigger: {
+    scrub: 1
   }
 })
 
 
 const timeline = gsap.timeline();
 
-timeline.from("#nav-menu input", {
-  y: "-52px",
-  duration: 0.5,
-  stagger: 0.4,
-});
-
 
 timeline.from("#hello", {
   x: -50,
-  duration: 0.6,
+  duration: 0.5,
   opacity: 0,
 });
 
@@ -157,21 +272,10 @@ timeline.from(".jobTitle", {
 });
 
 timeline.from(".salute-desc", {
-  opacity : 0,
-  duration : 0.6
+  opacity: 0,
+  duration: 0.6
 })
 
 timeline.from(".hire-button", {
-  opacity : 0
+  opacity: 0
 })
-
-timeline.from('#scroll-down', {
-  opacity : 0
-})
-
-timeline.to("#scroll-down", {
-  y: 20,
-  yoyo: true,
-  repeat: -1,
-  duration: 1.3,
-});
